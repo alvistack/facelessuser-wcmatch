20787471b242db94864bb025c0a7dc7bbbd12678
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-02-16 13:00:42 +0800

    alvistack/10.1
        git clean -xdf
        tar zcvf ../python-wcmatch_10.1.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-wcmatch.spec ../python-wcmatch_10.1-1.spec
        cp ../python*-wcmatch*10.1*.{gz,xz,spec,dsc} /osc/home\:alvistack/facelessuser-wcmatch-10.1/
        rm -rf ../python*-wcmatch*10.1*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='wcmatch',
+    version='10.1',
+    description='Wildcard/glob file name matcher.',
+    author_email='Isaac Muse <Isaac.Muse@gmail.com>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Typing :: Typed',
+    ],
+    install_requires=[
+        'bracex>=2.1.1',
+    ],
+    packages=[
+        'wcmatch',
+    ],
+)
